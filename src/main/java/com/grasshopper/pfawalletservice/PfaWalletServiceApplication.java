0bc3f48b5558502ddc4919edef3fb4f2cce67333
anisul islam <anis.2803@gmail.com>
2020-01-03 22:47:43 +0100

    Initial Commit

---
+++ src/main/java/com/grasshopper/pfawalletservice/PfaWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grasshopper.pfawalletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grasshopper/pfawalletservice/PfaWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grasshopper.pfawalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfaWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
